7a29df836fe5baa93d8899c38c2b0ed2bcfd078b
wjdwjdwjd1279 <alfzltm9721@naver.com>
2023-10-21 23:22:05 +0900

    7-5

--- main.c
+++ main.c
@@ -5,15 +5,21 @@
 
 int counter;
 
-void set_counter(void)
-{
-	counter = 20;	
+void sub(void);
+
+int main(void) {
+	int i;
+	for(i=0;i<3;i++)
+		sub();
+	return 0;
 }
 
-int main(int argc, char *argv[]) {
-	
-	printf("counter = %d \n",counter);
-	set_counter();
-	printf("counter=%d\n",counter);
-	
+void sub(void)
+{
+	int auto_count = 0;
+	static int static_count = 0;
+	auto_count++;
+	static_count++;
+	printf("auto_count=%d\n",auto_count);
+	printf("static_count=%d\n",static_count);
 }
