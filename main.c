6ac9bba956728d6f3b29c97fa095c12fda6aef8c
wjdwjdwjd1279 <alfzltm9721@naver.com>
2023-10-21 23:28:17 +0900

    7-6-(2)

--- main.c
+++ main.c
@@ -3,12 +3,13 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int all_files;
+static int all_files;
 
 extern void sub();
 
 int main(void) {
 	sub();
 	printf("%d\n",all_files);
+	
 	return 0;
 }
